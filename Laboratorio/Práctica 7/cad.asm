0c1ba476ef5110ddf50a117c5949276e4a455802
unknown <andiee.grr@gmail.com>
2020-05-13 13:15:07 -0500

    Ágregando Práctica 7 :)

--- Laboratorio/Práctica 7/cad.asm
+++ Laboratorio/Práctica 7/cad.asm
@@ -7,6 +7,7 @@
 ;#                                                                                                  #
 ;####################################################################################################
 
+      processor PIC16F886
       #INCLUDE <P16F886.INC>
       ORG 0
       ORG 5
@@ -15,13 +16,13 @@
       apuntador EQU 0x21
       tam EQU 0x23
 
-      DCounter1 EQU 0X0C
-      DCounter2 EQU 0X0D
-      DCounter3 EQU 0X0E
-      DCounter4 EQU 0X0C
+      DCounter1 EQU 0x24
+      DCounter2 EQU 0x25
+      DCounter3 EQU 0x26
+      DCounter4 EQU 0x27
 
 
-	    CBLOCK 0x24
+	    CBLOCK 0x28
 	       d1
 	       d2
 	       d3
@@ -35,10 +36,11 @@
       BSF TRISA,0 				;Configura como entrada el pin RA0
       ;----------------------------------------------------------------------------------------------
       ;-------------------------------------------------------------------------Configuración del CAD
-      CLRF ADCON1         ;Los voltajes de referencia serán internos y se usa justificación izquierda
+      CLRF ADCON1         ;Los voltajes de referencia serán internos y la justificación izquierda
       BSF STATUS,RP1 			;Banco 3
       BSF ANSEL,ANS0			;Configura el pin RA0 como entrada analógica
-      CLRF ANSELH
+      ;CLRF ANSELH
+
       BCF STATUS,RP0			;
       BCF STATUS,RP1      ;Banco 0
       MOVLW 0x41				 	;0100 0001 -> Selecciona el canal AN0, reloj de conversión Fosc/8
@@ -48,15 +50,15 @@
 MAIN:
       CLRF apuntador
       CALL DELAY_20_us          ;Espera tiempo de adquisición
-      BSF ADCON0,GO_NOT_DONE	  ;Con GO/DONE=1 se toma la muestra e inicia la conversión
+      BSF ADCON0,GO_NOT_DONE	  ;Con GO/DONE = 1 se toma la muestra e inicia la conversión
 ESP:
-      BTFSC ADCON0,GO_NOT_DONE  ;Espera a que termine la conversión (GO/DONE=0);
+      BTFSC ADCON0,GO_NOT_DONE  ;Espera a que termine la conversión (GO/DONE = 0);
       GOTO ESP
-
+   	  ;------------------------------------------Comparaciones para saber si el valor es > 3.5 V o <=
       MOVF ADRESH,W             ;Mueve W <- ADRESH
       MOVWF VALOR
-      MOVLW .716                ;716 para 3.5 V
-      SUBWF VALOR
+      MOVLW 0xB3                ;716 para 3.5 V
+      SUBWF VALOR,W
       BTFSS STATUS,C            ;¿A >= B? ¿C = 1?
       GOTO MENOR_IGUAL
 
@@ -64,30 +66,57 @@ MAYOR_IGUAL:
       BTFSC STATUS,Z            ;Ahora prueba ¿A > B? ¿Z = 0?
       GOTO MENOR_IGUAL
       GOTO MAYOR
-
+	  ;---------------------------------------------------------------------------------------------
+	  ;-----------------------------------------------------------------Si es menor o igual a 3.5...
 MENOR_IGUAL:
       BCF PORTB,0               ;Apagar el bit B0
 
-      MOVLW 6
+      MOVLW 6                   ;Enviar la palabra "abajo"
       MOVWF tam
+
+PRUEBA_MI:
       DECFSZ tam,f
-      CALL PALABRA_MENOR
+      GOTO INCREMENTA
+      MOVLW 0x0D
+      CALL TRANSMITE
+      MOVLW 0x0A
+      CALL TRANSMITE
       CALL DELAY_500_ms
       GOTO MAIN
 
+INCREMENTA:
+      CALL PALABRA_MENOR
+      CALL TRANSMITE
+      INCF apuntador,F
+      GOTO PRUEBA_MI
+	  ;---------------------------------------------------------------------------------------------
+	  ;-------------------------------------------------------------------------Si es mayor a 3.5...
 MAYOR:
       BSF PORTB,0               ;Encender el bit B0
-      MOVLW 7
+
+      MOVLW 7                   ;Enviar la palabra "arriba"
       MOVWF tam
+
+PRUEBA_M:
       DECFSZ tam,f
-      CALL PALABRA_MAYOR
+      GOTO INCREMENTA_M
+      MOVLW 0x0D
+      CALL TRANSMITE
+      MOVLW 0x0A
+      CALL TRANSMITE
       CALL DELAY_500_ms
       GOTO MAIN
-      ;----------------------------------------------------------------------------------------------
+
+INCREMENTA_M:
+      CALL PALABRA_MAYOR
+      CALL TRANSMITE
+      INCF apuntador,F
+      GOTO PRUEBA_M
+	  ;----------------------------------------------------------------------------------------------
       ;---------------------------Subrutina que inicializa el puerto serial y habilita la transmisión
 CONFIG_USART
       ;-Para 9600 Bauds con Fosc = 4 Mhz -> BRGH = 1, BRG16 = 0, SYNC = 0, SPRGH = 0x00 , SPRG = 0x19
-	    BSF STATUS,RP0 			;Pasamos al banco 1
+      BSF STATUS,RP0 			;Pasamos al banco 1
       BSF TXSTA,BRGH 			;Pone el bit BRGH = 1
       MOVLW 0x19			    ;Carga un 0x19 = 0001 1001 en SPRG
       MOVWF SPBRG 			  ;
@@ -118,14 +147,12 @@ PALABRA_MAYOR:
       MOVF apuntador,W		 ;Carga apuntador en W
       ADDWF PCL,1 			   ;Salta W instrucciones adelante
       DT "arriba"
-      INCF apuntador,F
       RETURN
 
 PALABRA_MENOR:
       MOVF apuntador,W		 ;Carga apuntador en W
       ADDWF PCL,1 			   ;Salta W instrucciones adelante
       DT "abajo"
-      INCF apuntador,F
       RETURN
       ;----------------------------------------------------------------------------------------------
       ;------------------------Subrutina que genera un retardo de 20 us para el tiempo de adquisición
@@ -155,4 +182,4 @@ LOOP
       NOP
       RETURN
       ;----------------------------------------------------------------------------------------------
-		  END
+	  END
